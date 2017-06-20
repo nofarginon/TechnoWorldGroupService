bf90d0b8103c7f2fbb551c61e9091f1690a13da4
nofarginon <nofar.ginon1@gmail.com>
2017-06-20 16:11:29 +0300

    'updatecontrol'

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(
     (req,res,next) => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers",``,
+        res.header("Access-Control-Allow-Headers",
             "Origin, X-Requested-With, Content-Type, Accept");
         res.set("Content-Type", "application/json");
         next();
