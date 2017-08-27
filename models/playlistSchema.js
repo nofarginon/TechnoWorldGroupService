3be43d969bf374c518c05ef20bba437cb7843910
oriamd <ori.amuday@gmail.com>
2017-08-27 19:12:45 +0300

    added comment and url to schema

--- models/playlistSchema.js
+++ models/playlistSchema.js
@@ -1,12 +1,14 @@
 var     mongoose = require (`mongoose`),
         schema = mongoose.Schema,
         song=require('./song_schema').songSchema;
+        comment=require('./comments_schema').commentsSchema;
         playlistSchema = new schema({
             Djname: {type: String,required: true},
             Djimg: {type: String},
             country:{type:String,required: true},
             about:{type:String,required: true},
-            songs:[song]
+            songs:[song],
+            comments:[comment]
     },{collection:`playlist`});
 
 var Playlist = mongoose.model(`playlist`, playlistSchema);
